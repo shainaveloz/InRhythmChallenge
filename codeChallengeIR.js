406e0c2bfb62a50e8cfb80396950838f9cf06d6c
shainaveloz <shaina.veloz@gmail.com>
2018-07-02 23:07:33 -0400

    Adding js file

---
+++ codeChallengeIR.js
@@ -0,0 +1,34 @@
+'use strict';
+
+class GitHubUser{
+  constructor(username){
+    this.username = username;
+  }
+
+  async fetchDetails(cb){
+    let self = this;
+    cb = cb || function(){};
+
+    if(this.userData){
+      setTimeout(()=>{
+        cb(self.userData);
+      },0);
+    }else{
+      let response = await fetch(`https://api.github.com/users/${this.username}`)
+      let user = await response.json();
+      user.then((data)=>{
+        self.data = data;
+        return cb(data);
+      })
+    }
+
+    async function getData(){
+      let data = await fetchDetails();
+      return data;
+    }
+  }
+}
+
+let octocat = new GitHubUser('octocat');
+
+octocat.fetchDetails(data =>{console.log(data)});
